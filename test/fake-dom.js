053d05883201dd9e64b27ccc8ee43e014b6b0ff8
Chung Leong <chernyshevsky@hotmail.com>
2023-02-24 00:29:49 +0100

    Making progress on code coverage.

--- test/fake-dom.js
+++ test/fake-dom.js
@@ -1,15 +1,12 @@
 export async function withFakeDOM(cb) {
-  const globalVars = {
-    navigator: new Navigator(),
-    window: new Window(),
-  };
+  const window = new Window();
   try {   
-    for (const [ name, value ] of Object.entries(globalVars)) {
+    for (const [ name, value ] of Object.entries(window)) {
       global[name] = value;
     }
     await cb();
   } finally {
-    for (const [ name, value ] of Object.entries(globalVars)) {
+    for (const [ name, value ] of Object.entries(window)) {
       delete global[name];
     }
   }
@@ -18,7 +15,9 @@ export async function withFakeDOM(cb) {
 class Window extends EventTarget {
   constructor() {
     super();
+    this.window = this;
     this.document = new Document();
+    this.navigator = new Navigator();
   }
 }
 
@@ -33,8 +32,32 @@ class Document extends EventTarget {
 class VideoElement extends EventTarget {
   constructor() {
     super();
+    this.playing = false;
     this.videoWidth = 0;
     this.videoHeight = 0;
+    this.srcObject = null;
+  }
+
+  play() {
+    if (this.srcObject) {
+      setTimeout(() => {
+        try {
+          this.srcObject.onPlay?.();
+          this.playing = true;
+          this.oncanplay?.({});  
+        } catch (err) {
+          this.onerror?.({ type: 'error', error: err });
+        }
+      }, 0);
+    }
+  }
+
+  pause() {
+    this.playing = false;
+  }
+
+  getTracks() {
+    return this.tracks;
   }
 }
 
@@ -53,15 +76,16 @@ class MediaDevices extends EventTarget {
   }
 
   async enumerateDevices() {
-    return this.devices.slice();
+    return this.devices.slice(0);
   }
 
-  addDevice(device) {
-    this.devices.push(device);
+  addDevice(info) {
+    this.devices.push(new MediaDeviceInfo(info));
+    this.notifyChange();
   }
 
-  removeDevice(device) {
-    const index = this.devices.indexOf(device);
+  removeDevice(deviceId) {
+    const index = this.devices.findIndex(d => d.deviceId === deviceId);
     if (index !== -1) {
       this.devices.splice(index, 1);
       this.notifyChange();
@@ -71,6 +95,66 @@ class MediaDevices extends EventTarget {
   notifyChange() {
     const evt = new Event('devicechange');
     this.dispatchEvent(evt);
+  }
+
+  async getUserMedia(constraints) {
+    this.onStreamRequest?.(constraints);
+    const devices = [];
+    for (const [ kind, value ] of Object.entries(constraints)) {
+      let device;
+      if (value.deviceId) {
+        device = this.devices.find(d => d.deviceId === value.deviceId);
+      }
+      if (!device) {
+        device = this.devices.find(d => d.kind === `${kind}input`);
+      }
+      if (device) {
+        devices.push(device);
+      }
+    }
+    if (devices.length === 0) {
+      throw new Error('Unable to find a device matching constraint');
+    } else {
+      const stream = new MediaStream(devices);
+      this.onStreamResponse?.(stream);
+      return stream;
+    }
+  }
+}
+
+class MediaDeviceInfo {
+  constructor(info) {
+    Object.assign(this, info);
+  }
+}
+
+class MediaStream extends EventTarget {
+  constructor(devices) {
+    super();
+    this.videoWidth = 0;
+    this.videoHeight = 0;
+    this.tracks = devices.map(device => new MediaStreamTrack(device));
+  }
+
+  getTracks() {
+    return this.tracks;
+  }
+}
+
+class MediaStreamTrack extends EventTarget {
+  constructor(device) {
+    super();
+    if (device.kind === 'videoinput') { 
+      this.kind = 'video';
+    } else if (device.kind === 'audioinput') {
+      this.kind = 'audio';
+    }
+    this.device = device;
+    this.stopped = false;
+  }
+
+  stop() {
+    this.stopped = true;
   }
 }
 
--- test/hooks.test.js
+++ test/hooks.test.js
@@ -23,25 +23,86 @@ describe('Hooks', function() {
         await render(el);
       });
     })
-    it('should work with fake DOM', async function() {
+    it('should fail to get a stream when there are not devices', async function() {
       await withFakeDOM(async () => {
         await withTestRenderer(async ({ render }) => {
           let state;
           function Test() {
-            try {
-              state = useMediaCapture({
-                video: true,
-              });
-              return null;
-                
-            } catch (err) {
-              console.error(err);
-            }
+            state = useMediaCapture({
+              video: true,
+            });
+            return null;                
           }
           const el = createElement(Test);
           await render(el);
-        }); 
+          await delay(10);
+          const { status, lastError } = state;
+          expect(status).to.equal('denied');
+          expect(lastError).to.be.an('error');
+        });
       });      
     })
+    it('should reach the state of previewing when there is a device', async function() {
+      await withFakeDOM(async () => {
+        navigator.mediaDevices.addDevice({
+          deviceId: '007',
+          groupId: '007',
+          kind: 'videoinput',
+          label: 'Sky camera',
+        });
+        await withTestRenderer(async ({ render }) => {
+          let state;
+          function Test() {
+            state = useMediaCapture({
+              video: true,
+            });
+            return null;                
+          }
+          const el = createElement(Test);
+          await render(el);
+          await delay(10);
+          const { status, lastError } = state;
+          expect(status).to.equal('previewing');
+          expect(lastError).to.be.undefined;
+        });
+      });
+    })
+    it('should request device matching preferredDevice', async function() {
+      await withFakeDOM(async () => {
+        navigator.mediaDevices.addDevice({
+          deviceId: '00001-1',
+          groupId: '00001',
+          kind: 'videoinput',
+          label: 'Back facing camera',
+        });
+        navigator.mediaDevices.addDevice({
+          deviceId: '00002-1',
+          groupId: '00002',
+          kind: 'videoinput',
+          label: 'Front facing camera',
+        });
+        await withTestRenderer(async ({ render }) => {
+          let state;
+          function Test() {
+            state = useMediaCapture({
+              video: true,
+              audio: false,
+              preferredDevice: 'front',
+            });
+            return null;                
+          }
+          const el = createElement(Test);
+          await render(el);
+          await delay(10);
+          const { liveVideo, status, lastError } = state;
+          expect(liveVideo).to.not.be.undefined;
+          const { stream } = liveVideo;
+          const [ track ] = stream.getTracks();
+          expect(track.device).to.have.property('deviceId', '00002-1');
+          expect(status).to.equal('previewing');
+          expect(lastError).to.be.undefined;
+        });
+      });
+    });      
   })
 })
