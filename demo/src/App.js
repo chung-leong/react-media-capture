69467bd0165601cf797af79a7ebd9c54d49da0a1
Chung Leong <chernyshevsky@hotmail.com>
2023-03-13 00:29:12 +0100

    Implemented saving to file to demo.

--- demo/src/App.js
+++ demo/src/App.js
@@ -8,10 +8,20 @@ import ErrorBoundary from './ErrorBoundary.js';
 export default function App() {
   const [ selection, setSelection ] = useState(null);
   const onClose = useCallback(() => setSelection(), []);
-  const onCapture = useCallback((result) => {
+  const onCapture = useCallback(async (result) => {
     console.log(result);
     setSelection();
-  }, []);
+    const ext = { video: '.webm', audio: '.webm', photo: '.jpg' }[selection];
+    const suggestedName = new Date().toISOString().slice(0, -5) + ext;
+    const { blob } = result;
+    const accept = {};
+    const mimeType = blob.type.split(';')[0];
+    accept[mimeType] = ext;
+    const file = await window.showSaveFilePicker({ suggestedName, types: [ { accept } ] });
+    const stream = await file.createWritable();
+    await stream.write(blob);
+    await stream.close();
+  }, [ selection ]);
   return (
     <div>
       <div>
